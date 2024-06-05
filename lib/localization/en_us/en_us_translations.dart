a67819548e732354cd387072988144aed69998da
nonyonah <nonyonah@gmail.com>
2024-06-05 12:26:44 +0000

    added localization and translations

---
+++ lib/localization/en_us/en_us_translations.dart
@@ -0,0 +1,12 @@
+final Map<String, String> enUS = {
+  // Register Screen
+  "lbl_continue": "Continue",
+  "lbl_email_address": "Email address",
+  "lbl_password": "Password",
+  "lbl_register": "Register",
+  "msg_enter_your_email": "Enter your email",
+
+  //Network errors
+  "msg_network_err": "Network error",
+  "msg_something_went_wrong": "Something went wrong",
+};
