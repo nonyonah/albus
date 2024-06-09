30c9dae0f5fab697e145805e5490c31fef3c7cc1
nonyonah <nonyonah@gmail.com>
2024-06-09 15:32:34 +0000

    added more routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:myapp/screens/register_screen/register_screen.dart';
+import 'package:myapp/screens/splash_screen/splash_screen.dart';
 
 class AppRoutes {
+  static const String splashScreen = '/splash_screen';
+
+  static const String onboardingScreen = '/onboarding_screen';
+  
   static const String registerScreen = '/register_screen';
 
   static const String initialRoute = '/initialRoute';
 
   static Map<String, WidgetBuilder> get routes => {
+    splashScreen: SplashScreen.builder,
+    onboardingScreen: OnboardingScreen.builder,
     registerScreen: RegisterScreen.builder,
     initialRoute: RegisterScreen.builder
   };
