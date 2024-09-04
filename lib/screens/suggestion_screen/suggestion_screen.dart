6c6638f10e60dfa56f8dd98940bbd5bc05ed1cdf
nonyonah <nonyonah@gmail.com>
2024-09-04 00:09:57 +0000

    feat: added suggestion screen and added the ability for selected catgories to appear in the suggestions screen

--- lib/screens/suggestion_screen/suggestion_screen.dart
+++ lib/screens/suggestion_screen/suggestion_screen.dart
@@ -1,5 +1,6 @@
 import 'package:albus/core/utils/image_constant.dart';
 import 'package:albus/core/utils/size_utils.dart';
+import 'package:albus/widgets/custom_elevated_button.dart';
 import 'package:albus/widgets/custom_image_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,7 +34,30 @@ class _SuggestionScreenState extends ConsumerState<SuggestionScreen> {
                 height: 24.h,
                 width: 24.h,
                 alignment: Alignment.center,
-              )
+                onTap: () {
+                  onTapGoBack(context);
+                },
+              ),
+              SizedBox(height: 48.h),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Let's allocate some amount",
+                  style: theme.textTheme.headlineLarge,
+                ),
+              ),
+              SizedBox(height: 18.h),
+              _buildAllocationSuggestions(context),
+              SizedBox(height: 10.h),
+              _buildCurrencyDisplayContext(context),
+              SizedBox(height: 10.h),
+              _buildShoppingList(context),
+              SizedBox(height: 244.h),
+              CustomElevatedButton(
+                text: 'Finish',
+                margin: EdgeInsets.symmetric(horizontal: 10.h),
+              ),
+              SizedBox(height: 30.h)
             ],
           ),
         )
